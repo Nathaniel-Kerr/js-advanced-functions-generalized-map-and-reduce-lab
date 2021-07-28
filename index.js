7cce45f35044135367330cf041f4f6708d626f26
Nathaniel <NathanielKerr10@gmail.com>
2021-07-28 14:27:03 -0700

    commit tree ef2a7028766c196af0421cf5e74617c075ce4815

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, callback) { 
+   let newArray = []
+   for (let i = 0; i < array.length; i++) {
+       let element = array[i]
+       newArray.push(callback(element))
+   }
+   return newArray;
+}
+
+function reduce(array, callback, value) {
+    let a = (!!value) ? value : array[0]
+    let i = (!!value) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        a = callback(array[i], a)
+    }
+    return a
+}
